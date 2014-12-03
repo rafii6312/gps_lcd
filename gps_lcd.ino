cca43656e98899cb9b0a745e1b89abd1b1faa099
rafii6312 <rafig.ossami@googlemail.com>
2014-12-03 22:35:12 +0100

    some fixes
    >_

--- gps_lcd.ino
+++ gps_lcd.ino
@@ -2,7 +2,10 @@
 #include <TinyGPS.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(2, 3, 4, 5, 11, 12);
+
+
 TinyGPS gps;
 SoftwareSerial ss(6, 7);
 
@@ -11,48 +14,66 @@ void setup()
   Serial.begin(9600);
   ss.begin(9600);
   lcd.begin(16, 2);
-  
+
   printLcd(0, "waiting for data");
-  printLcd(1, "................");
+  printLcd(1, "                ");
 }
 
 void loop()
 {
-  bool newData = false;
-  unsigned long chars;
-  unsigned short sentences, failed;
+  bool newdata = false;
+  unsigned long start = millis();
 
-  for (unsigned long start = millis(); millis() - start < 1000;)
+  // Every second we print an update
+  while (millis() - start < 1000)
   {
-    while (ss.available())
-    {
-      char c = ss.read();
-      if (gps.encode(c)) newData = true;
-    }
+    if (feedgps())
+      newdata = true;
   }
 
-  if (newData)
+  if (newdata)
   {
-    
+
     String sflat, sflon;
     float flat, flon;
-    unsigned long age;
+    unsigned long age, date, time, chars = 0;
+    unsigned short sentences = 0, failed = 0;
+
+
+    flat = TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat;
+    flon = TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon;
+
+    //flat = gps.
+
     gps.f_get_position(&flat, &flon, &age);
-    
-    sflat.concat(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat);
-    sflon.concat(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon);
-    
-    printLcd(0, "lat:" + sflat);
-    printLcd(1, "lon:" + sflon);
-        
-    
-  } else {
+
+    //sflat.concat(flat);
+    //sflon.concat(flon);
+
+    printLcd(0, toString(flat, "lat: "));
+    printLcd(1, toString(flon, "lon: "));
+
+
+  } 
+  else {
     //no new data
+    //printLcd(0, "waiting for data");
+    //printLcd(1, millis() / 1000);
   }
-  gps.stats(&chars, &sentences, &failed);
+  //gps.stats(&chars, &sentences, &failed);
 }
 
 void printLcd(int line, String txt)
+{
+  clearLcd(line);
+  lcd.setCursor(0, line);
+  lcd.print(txt);
+  Serial.print(line);
+  Serial.print(") ");
+  Serial.println(txt);
+}
+
+void printLcd(int line, float txt)
 {
   clearLcd(line);
   lcd.setCursor(0, line);
@@ -67,6 +88,25 @@ void clearLcd(int line)
   lcd.setCursor(0, line);
   lcd.print("                ");
 }
+
+static bool feedgps()
+{
+  while (ss.available())
+  {
+    if (gps.encode(ss.read()))
+      return true;
+  }
+  return false;
+}
+
+String toString(float f, String pre)
+{
+  char tmp[16];
+  dtostrf(f,1,4,tmp);
+  String strOut = pre + tmp;
+  return strOut;
+}
+
 
 
 
